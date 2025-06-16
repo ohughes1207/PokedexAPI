df8a69cf0f92e979f2702203d03a08e6a3888644
Olliver Hughes <ohughes1207@gmail.com>
2025-06-16 11:12:48 +0100

    Base DTO and ClassMap for DTO

---
+++ PokedexAPI/DTOs/PokemonBaseCsvDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace PokedexAPI.DTOs
+{
+    public class PokemonBaseCsvDto
+    {
+        public int PokedexNum { get; set; }
+        public required string BaseName { get; set; }
+        public int Generation { get; set; }
+        public bool Legendary { get; set; }
+        public bool Paradox { get; set; }
+        public bool PseudoLegendary { get; set; }
+        public bool Ultrabeast { get; set; }
+        public bool Mythical { get; set; }
+    }
+}
---
+++ PokedexAPI/Mappers/PokemonBaseCsvDtoMap.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration;
+using PokedexAPI.DTOs;
+
+namespace PokedexAPI.Mappers
+{
+    public class PokemonBaseCsvDtoMap : ClassMap<PokemonBaseCsvDto>
+    {
+        public PokemonBaseCsvDtoMap()
+        {
+            Map(m => m.PokedexNum).Name("Pokedex Number");
+            Map(m => m.BaseName).Name("Base Pokemon");
+            Map(m => m.Generation).Name("Generation");
+            Map(m => m.Paradox).Name("Paradox");
+            Map(m => m.Legendary).Name("Legendary");
+            Map(m => m.PseudoLegendary).Name("Pseudo-legendary");
+            Map(m => m.Ultrabeast).Name("Ultrabeast");
+            Map(m => m.Mythical).Name("Mythical");
+        }
+    }
+}
